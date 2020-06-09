dbe9fac4063ca1281ec5e005ee239f5960a2c663
phong190897 <phong190897@gmail.com>
2020-06-10 00:30:22 +0700

    commit code

--- WebThiTracNghiem/Controllers/HomeController.cs
+++ WebThiTracNghiem/Controllers/HomeController.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebThiTracNghiem.Areas.Admin.Controllers;
+using WebThiTracNghiem.Areas.Admin.Data;
+using WebThiTracNghiem.Areas.Admin.Utils;
 
 namespace WebThiTracNghiem.Controllers
 {
-    public class HomeController : Controller
+    [Authorize]
+
+    public class HomeController : BaseController
     {
+        MonRepository _monThiRepo = new MonRepository();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult ThiThu()
         {
-            ViewBag.Message = "Your application description page.";
+            List<MonModel> listMonThi = _monThiRepo.GetMonThiToDropDownList();
+            var monThi = new List<SelectListItem>();
+
+            monThi.AddRange(from a in listMonThi
+                            select new SelectListItem
+                            {
+                                Text = a.TenMon,
+                                Value = a.MaMon
+                            });
+
+            if (listMonThi.Count != 0)
+            {
+                ViewBag.MonThi = monThi;
+            }
 
             return View();
         }
---
+++ WebThiTracNghiem/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebThiTracNghiem.Areas.Admin.Code;
+using WebThiTracNghiem.Areas.Admin.Models;
+using WebThiTracNghiem.Areas.Admin.Utils;
+
+namespace WebThiTracNghiem.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            if (Membership.ValidateUser(model.TenTaiKhoan, model.MatKhau) && ModelState.IsValid)
+            {
+                FormsAuthentication.SetAuthCookie(model.TenTaiKhoan, true);
+                var taikhoanRepo = new TaiKhoanRepository();
+                var userInfo = taikhoanRepo.getTaiKhoanInfo(model.TenTaiKhoan);
+                var userSession = new UserSession();
+                userSession.UserName = model.TenTaiKhoan;
+                userSession.Quyen = userInfo.Data.Select(m => m.MaQuyen).FirstOrDefault();
+                Session.Add(Constance.Constance.USER_SESSION, userSession);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng");
+                if (string.IsNullOrEmpty(model.TenTaiKhoan))
+                {
+                    ModelState.AddModelError("", "Tên đăng nhập không được để trống");
+                }
+
+                if (string.IsNullOrEmpty(model.MatKhau))
+                {
+                    ModelState.AddModelError("", "Mật khẩu không được để trống");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
